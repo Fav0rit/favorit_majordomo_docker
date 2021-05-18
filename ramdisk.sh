751ba941af8c846224d58b8235f90f4d15476de4
Paul Gridin <pfavorit@gmail.com>
2021-05-18 23:47:15 +0300

    add ramdisk scripts

---
+++ ramdisk.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+path_mysql_tmp="/opt/favorit_majordomo_docker/app/data/mysql"
+
+docker-compose down
+
+sudo mv ./app/data/mysql ./app/data/mysql-real
+sudo ln -s ./config/ramdisk /etc/init.d/ramdisk
+sudo update-rc.d ramdisk defaults
+
+echo "tmpfs $path_mysql_tmp tmpfs size=350m 0 0" >> /etc/fstab
+echo "*/10 * * * * root /etc/init.d/ramdisk sync >> /dev/null 2>&1" >> /etc/crontab
+
+docker-compose up -d
+#sudo reboot
