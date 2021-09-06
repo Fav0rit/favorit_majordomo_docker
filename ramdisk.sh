7b4d4ccf76f4e2cba806f148765b1bd116b9be66
Paul Gridin <pfavorit@gmail.com>
2021-09-07 01:20:57 +0300

    updates
    Добавлена полноценная поддержка armv7 для raspberry и подобных систем

--- ramdisk.sh
+++ ramdisk.sh
@@ -5,6 +5,7 @@ path_mysql_tmp="/opt/favorit_majordomo_docker/app/data/mysql"
 docker-compose down
 
 sudo mv ./app/data/mysql ./app/data/mysql_real
+sudo chmod 755 ./config/ramdisk
 sudo cp ./config/ramdisk /etc/init.d/ramdisk
 sudo update-rc.d ramdisk defaults
 
